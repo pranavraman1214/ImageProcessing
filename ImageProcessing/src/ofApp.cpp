58f57f58b0e4add2eef59ee2d1ff1887de808cdc
pranavraman <pvraman2@illinois.edu>
2019-07-14 14:14:22 -0500

    It highlights faces. However, since I am using the frontal cat face data that was provided by OpenCV. The person has to be staring at the camera straight ahead. But I will use the other training data to highlight other faces

--- ImageProcessing/src/ofApp.cpp
+++ ImageProcessing/src/ofApp.cpp
@@ -13,9 +13,17 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofimgsrc.draw(0, 0, ofGetWindowWidth(), ofGetWindowHeight());
+    ofimgsrc.draw(0, 0);
     for (int i = 0; i < face_x_coordinates.size(); i++) {
+        ofColor(255,255,255);
+        ofNoFill();
         ofDrawEllipse(face_x_coordinates.at(i), face_y_coordinates.at(i), face_width.at(i), face_height.at(i));
+        //ofDrawCircle(face_x_coordinates.at(i), face_y_coordinates.at(i), face_width.at(i));
+        
+        std::cout <<"X Coordinate: " <<face_x_coordinates.at(i) << std:: endl;
+        std::cout <<"Y Coordinate: "<<face_y_coordinates.at(i) << std:: endl;
+        std::cout <<"Face Width: "<<face_width.at(i) << std:: endl;
+        std::cout <<"Face Height: "<<face_height.at(i) << std:: endl;
     }
 }
 void ofApp::windowResized(int w, int h) {
@@ -29,10 +37,9 @@ void ofApp::keyPressed(int key){
         ofFileDialogResult loadFileResult = ofSystemLoadDialog();
         string path = loadFileResult.getPath();
         ofimgsrc.load(path);
-        imgsrc = ofxCv :: toCv(ofimgsrc);
-        //resize();
+        resize();
     }
-    if (key == 'i') {
+    if (key == 'a') {
         image_sharpening();
     }
     if (key == 'f') {
@@ -43,10 +50,10 @@ void ofApp:: resize() {
     for (float i = 1.0 ; i > 0; i-=.1) {
         if (i * ofimgsrc.getHeight() <= ofGetScreenHeight() && i * ofimgsrc.getWidth() <= ofGetScreenWidth()) {
             ofSetWindowShape(i*ofimgsrc.getWidth(), i * ofimgsrc.getHeight());
+            ofimgsrc.resize(i * ofimgsrc.getWidth(), i* ofimgsrc.getHeight());
             huge_image = false;
             altering_factor = i;
-            cv:: resize(imgsrc, imgsrc, cv::Size(i * ofimgsrc.getWidth(), i * ofimgsrc.getHeight()));
-            ofimgsrc.update();
+            imgsrc = ofxCv :: toCv(ofimgsrc);
             break;
         }
     }
@@ -67,13 +74,14 @@ void ofApp::facialDetection() {
 face_image.load("/Users/pranavraman/Documents/PersonalProjects/ImageProcessing/ImageProcessing/data/haarcascades/haarcascade_frontalcatface.xml");
     cv:: Mat grayscale_image;
     cv:: cvtColor(imgsrc, grayscale_image, cv::COLOR_BGR2GRAY);
-    face_image.detectMultiScale(grayscale_image, faces, 1.1, 2, 0|cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30) );
+    face_image.detectMultiScale(grayscale_image, faces, 1.1, 2,
+                                0|cv::CASCADE_SCALE_IMAGE, cv::Size(10, 10) );
     for (size_t i = 0; i < faces.size(); i++) {
         cv::Rect current_rectangle = faces[i];
-        face_x_coordinates.push_back(current_rectangle.x * altering_factor);
-        face_y_coordinates.push_back(current_rectangle.y * altering_factor);
-        face_height.push_back(current_rectangle.height * altering_factor);
-        face_width.push_back(current_rectangle.width * altering_factor);
+        face_x_coordinates.push_back(current_rectangle.x + current_rectangle.width * .5);
+        face_y_coordinates.push_back(current_rectangle.y + current_rectangle.height * .5);
+        face_height.push_back(current_rectangle.height);
+        face_width.push_back(current_rectangle.width);
     }
     
 }
